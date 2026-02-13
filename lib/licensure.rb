6855101e17613ed522bcaf3e68d9b614ad4cff70
ydah <t.yudai92@gmail.com>
2026-02-14 01:03:17 +0900

    Initial commit

---
+++ lib/licensure.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "licensure/version"
+
+module Licensure
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/licensure_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Licensure do
+  it "has a version number" do
+    expect(Licensure::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
